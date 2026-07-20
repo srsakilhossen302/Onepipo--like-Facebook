076d9223408ccd4a4dd253c7be8f4a9a5d080540
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-07-21 02:48:26 +0600

    feat: initialize Android and iOS project configuration files for Flutter build environment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.onepipo"
+    namespace = "com.onepipo.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.onepipo"
+        applicationId = "com.onepipo.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/onepipo/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/onepipo/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.onepipo
+package com.onepipo.app
 
 import io.flutter.embedding.android.FlutterActivity
 
